2117af9a899e9d40b53b5f9df9b757c9d6e9d6f7
David Buxton <david@gasmark6.com>
2015-10-04 23:17:17 +0100

    Include the README and LICENSE in the source distribution.
    If you have pypandoc installed, the markdown is converted to reStructuredText
    so it looks nicer on PyPI.

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from distutils.core import setup
 import notrequests
 
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md', 'rb').read()
+
+
 setup(
     name='notrequests',
     version=notrequests.__version__,
     description='Like Requests, but using urllib2.',
-    long_description='A Python wrapper for the built-in urllib2 module, compatible with Requests, intended for use on Google App Engine.',
+    long_description=long_description,
     url = 'https://github.com/davidwtbuxton/notrequests',
     author='David Buxton',
     license='MIT',
